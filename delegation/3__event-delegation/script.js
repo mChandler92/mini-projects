27d3f43d530d426e347f563cc6e8901dcb401095
mChandler92 <142733187+mChandler92@users.noreply.github.com>
2023-10-31 23:47:52 -0500

    Delete delegation directory

--- delegation/3__event-delegation/script.js
+++
@@ -1,8 +0,0 @@
-// Write your code below
-const myList = document.getElementById("myList")
-
-myList.addEventListener("click", function (event) {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("selected")
-  }
-})
